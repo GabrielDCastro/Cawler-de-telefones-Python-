96567c2492b18281128520044a1b9946d85320ca
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2019-11-26 20:55:11 -0300

    encontrando links de anúncios

--- Crawler.py
+++ Crawler.py
@@ -1,7 +1,9 @@
 import requests
-from bs4 import BeautifulSoup #biblioteca para fazer parsing de html
+from bs4 import BeautifulSoup  # biblioteca para fazer parsing de html
 
+dominio = 'https://django-anuncios.solyd.com.br/'
 url = 'https://django-anuncios.solyd.com.br/automoveis/'
+
 
 def buscar(url):
     try:
@@ -9,18 +11,33 @@ def buscar(url):
         if resposta.status_code == 200:
             return resposta.text
         else:
-            print("Erro ao fazer a requisição")
+            print("Error ao fazer a requisição")
     except:
         print("Erro ao fazer a requisição")
 
 
 def parsing(resposta_html):
     try:
-        soup = BeautifulSoup(resposta, 'html.parser')
+        soup = BeautifulSoup(resposta_html, 'html.parser')
         return soup
     except:
         print("Erro ao fazer o parsing html")
-        
+
+def encontrar_links(soup):
+    cards_pai = soup.find("div", class_="ui three doubling link cards")
+    cards = cards_pai.find_all("a")
+
+    links = []
+    for card in cards:
+        link = card['href']
+        links.append(link)
+
+    return links
+
+
 resposta = buscar(url)
 if resposta:
     soup = parsing(resposta)
+    if soup:
+        links = encontrar_links(soup)
+        print(links)
