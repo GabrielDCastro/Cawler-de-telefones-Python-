e0e35905f3c495eaec0965b8257b7c564d82e68d
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2019-11-27 21:46:22 -0300

    threading, otimização do código e salvando telefones

--- Crawler.py
+++ Crawler.py
@@ -1,9 +1,14 @@
-import requests
-import re #expressão regular
-from bs4 import BeautifulSoup  # biblioteca para fazer parsing de html
+import re
+import threading
 
-dominio = "https://django-anuncios.solyd.com.br"
-url = "https://django-anuncios.solyd.com.br/automoveis/"
+import requests
+from bs4 import BeautifulSoup
+
+DOMINIO = "https://django-anuncios.solyd.com.br"
+URL_AUTOMOVEIS = "https://django-anuncios.solyd.com.br/automoveis/"
+
+LINKS = []
+TELEFONES = []
 
 
 def requisicao(url):
@@ -12,9 +17,10 @@ def requisicao(url):
         if resposta.status_code == 200:
             return resposta.text
         else:
-            print("Error ao fazer a requisição")
-    except:
-        print("Erro ao fazer a requisição")
+            print("Erro ao fazer requisição")
+    except Exception as error:
+        print("Erro ao fazer requisição")
+        print(error)
 
 
 def parsing(resposta_html):
@@ -44,25 +50,66 @@ def encontrar_links(soup):
 
     return links
 
+
 def encontrar_telefone(soup):
     try:
         descricao = soup.find_all("div", class_="sixteen wide column")[2].p.get_text().strip()
-    #olhando o site vemos que a parte inde se encontra o telefone está na class "sixteen wide column"
-    #mas existe 3 sixteen wide column e o telefone está na terceira "[2]"
-    #Além disso, ela está dentro de uma tag <p>
+        #olhando o site vemos que a parte inde se encontra o telefone está na class "sixteen wide column"
+        #mas existe 3 sixteen wide column e o telefone está na terceira "[2]"
+        #Além disso, ela está dentro de uma tag <p>
     except:
-        print("erro ao encontrar descrição")
+        print("Erro ai encontrar descricao")
+        return None
 
     regex = re.findall(r"\(?0?([1-9]{2})[ \-\.\)]{0,2}(9[ \-\.]?\d{4})[ \-\.]?(\d{4})", descricao)
     if regex:
         return regex
 
-resposta_busca = requisicao(url)
-if resposta_busca:
-    soup_busca = parsing(resposta_busca)
-    if soup_busca:
-        links = encontrar_links(soup_busca)
-        resposta_anuncio = requisicao(dominio + links[11])
+
+def descobrir_telefones():
+    while True:
+        try:
+            link_anuncio = LINKS.pop(0)
+        except:
+            return None
+
+        resposta_anuncio = requisicao(DOMINIO + link_anuncio)
+
         if resposta_anuncio:
             soup_anuncio = parsing(resposta_anuncio)
-            print(soup_anuncio.prettify())
+            if soup_anuncio:
+                telefones = encontrar_telefone(soup_anuncio)
+                if telefones:
+                    for telefone in telefones:
+                        print("Telefone encontrado:", telefone)
+                        TELEFONES.append(telefone)
+                        salvar_telefone(telefone)
+
+
+def salvar_telefone(telefone):
+    string_telefone = "{}{}{}\n".format(telefone[0], telefone[1], telefone[2])
+    try:
+        with open("telefones.csv", "a") as arquivo:
+            arquivo.write(string_telefone)
+    except Exception as error:
+        print("Erro ao salvar arquivo")
+        print(error)
+
+
+if __name__ == "__main__":
+    resposta_busca = requisicao(URL_AUTOMOVEIS)
+    if resposta_busca:
+        soup_busca = parsing(resposta_busca)
+        if soup_busca:
+            LINKS = encontrar_links(soup_busca)
+
+            THREADS = []
+            for i in range(10):
+                t = threading.Thread(target=descobrir_telefones)
+                THREADS.append(t)
+
+            for t in THREADS:
+                t.start()
+
+            for t in THREADS:
+                t.join()
